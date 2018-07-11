bf95fb8c8b812b06879beb84d9832d7aceea047e
Javier Ventajas Hernandez <javier.ventajas@scout24.ch>
2018-07-11 09:01:26 +0200

    Created basic project structure

---
+++ Game/GamePresenter.h
@@ -0,0 +1,14 @@
+//
+// Created by Ventajas Javier on 10.07.18.
+//
+
+#ifndef SNAKE_PLAYVIEWPRESENTER_H
+#define SNAKE_PLAYVIEWPRESENTER_H
+
+
+class PlayViewPresenter {
+
+};
+
+
+#endif //SNAKE_PLAYVIEWPRESENTER_H
---
+++ Game/GameView.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ventajas Javier on 10.07.18.
+//
+
+#include "PlayView.h"
